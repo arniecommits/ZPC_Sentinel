f973b711ad8e20b1847965e69ac0f5082bd4e44e
Arnab Roy <60926235+arniecommits@users.noreply.github.com>
2023-02-25 22:59:32 +0000

    Update

---
+++ CheckIPPort/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import nmap
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    name = req.params.get('name')
+    ip = name
+    if ip:
+        port = check_open_ports(ip)
+        if port:
+            return func.HttpResponse(body=f'{port}',status_code=200)
+        else:
+            return func.HttpResponse(body=f'No open ports detected on {ip}',status_code=404)    
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+def check_open_ports(ip_address):
+    nm = nmap.PortScanner()
+    result = nm.scan(ip_address, arguments="-p 1-1024")
+    for port in result["scan"][ip_address]["tcp"]:
+        if result["scan"][ip_address]["tcp"][port]["state"] == "open":
+            logging.info("Port " + port + " is open on " + ip_address)
+            return port
+        else:
+            exit    
