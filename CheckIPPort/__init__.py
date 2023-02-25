8361ee441971d63ae282dce7d89ff4151354b94f
Arnab Roy <60926235+arniecommits@users.noreply.github.com>
2023-02-25 23:24:32 +0000

    update

--- CheckIPPort/__init__.py
+++ CheckIPPort/__init__.py
@@ -1,9 +1,9 @@
 import logging
-import nmap
+import socket
 import azure.functions as func
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def ok(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     name = req.params.get('name')
     ip = name
@@ -18,12 +18,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
 def check_open_ports(ip_address):
-    nm = nmap.PortScanner()
-    result = nm.scan(ip_address, arguments="-p 1-1024")
-    for port in result["scan"][ip_address]["tcp"]:
-        if result["scan"][ip_address]["tcp"][port]["state"] == "open":
-            logging.info("Port " + port + " is open on " + ip_address)
-            return port
-        else:
-            exit    
+    open_ports = []
+    for port in range(1, 1024):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((ip_address, port))
+        if result == 0:
+            open_ports.append(port)
+    if open_ports:
+        return str(open_ports)
+    else:
+        print("No open ports found on " + ip_address)
+          
