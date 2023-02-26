fe5870e5d0be9566cafac4c306995ed2ccd055e4
Arnab Roy <60926235+arniecommits@users.noreply.github.com>
2023-02-26 00:56:08 +0000

    Timeout

--- CheckIPPort/__init__.py
+++ CheckIPPort/__init__.py
@@ -13,7 +13,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         if port:
             return func.HttpResponse(body=f'{port}',status_code=200)
         else:
-            return func.HttpResponse(body=f'No open ports detected on {ip}',status_code=404)    
+            return func.HttpResponse(body=f'{"status":"No open ports"}',status_code=200)    
     else:
         return func.HttpResponse(
              f"Request body {ip}",
@@ -24,7 +24,7 @@ def check_open_ports(ip_address):
     open_ports = []
     for port in (22,80,443,8080,3389,1433,1521,135,445,139,53):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+        sock.settimeout(0.5)
         result = sock.connect_ex((ip_address, port))
         if result == 0:
             open_ports.append(port)
