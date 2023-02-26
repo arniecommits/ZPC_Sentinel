cb542341505ccc1314c2368ed9854665633738c1
Arnab Roy <60926235+arniecommits@users.noreply.github.com>
2023-02-26 23:24:03 +0000

    Update

--- CheckIPPort/__init__.py
+++ CheckIPPort/__init__.py
@@ -1,34 +1,45 @@
 import logging
 import socket
 import azure.functions as func
+import json
+import asyncio
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    ip = req.get_body().decode('utf8')
-
-    logging.info(f'Got request body {ip}')
+    ip =""
+    if(req.get_body().decode('utf8')):
+        ip = req.get_body().decode('utf8')
+    else:
+        ip = req.params.get("ip")    
+        logging.info(f'Got request body {ip}')
     if ip:
         port = check_open_ports(ip)
-        if port:
-            return func.HttpResponse(body=f'{port}',status_code=200)
+        port = str(asyncio.run(port))
+        logging.info(f'Ports....... {port}')
+        func.HttpResponse(f'Found the following ports open {port}',200)
+        if (port):
+            return func.HttpResponse(port,200)
         else:
-            return func.HttpResponse(body=f'{"status":"No open ports"}',status_code=200)    
+            json_resp='{"ip_status":"No open ports"}'
+            json_b=json.dumps(json_resp)
+            return func.HttpResponse(json_b,200)    
     else:
         return func.HttpResponse(
-             f"Request body {ip}",
-             status_code=200
+             "No params passed in request",
+             200
         )
 
-def check_open_ports(ip_address):
+async def check_open_ports(ip_address):
     open_ports = []
     for port in (22,80,443,8080,3389,1433,1521,135,445,139,53):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
+        sock.settimeout(0.1)
         result = sock.connect_ex((ip_address, port))
         if result == 0:
             open_ports.append(port)
     if open_ports:
         return str(open_ports)
     else:
-        print("No open ports found on " + ip_address)
+        logging.info("No open ports found on " + ip_address)
+        return None
